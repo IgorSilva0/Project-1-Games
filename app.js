d3a515e57220e289bf624fdbe081eda71bac8c73
Igor Silva <igorsilvauk@gmail.com>
2024-03-10 12:17:01 +0000

    Middleware log added to app.js

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
+// using middleware to log requests
+app.use((req, res, next) => {
+  console.log(`Method : ${req.method}, Path : ${req.path}`);
+  next();
+});
+
+// Server listen to a port
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
