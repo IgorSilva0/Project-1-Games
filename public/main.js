a643f093d1bf1a03acde25a103ba64495bc86f96
Igor Silva <igorsilvauk@gmail.com>
2024-03-10 18:50:07 +0000

    imgs for RPS game added and styles edited, main.js changed DOM selector

--- public/main.js
+++ public/main.js
@@ -2,10 +2,19 @@ const startbtn = document.querySelector("#startbtn");
 const rockbtn = document.querySelector("#rock");
 const paperbtn = document.querySelector("#paper");
 const scissorbtn = document.querySelector("#scissor");
+const allRps = document.querySelectorAll(".rps");
+
+const block = document.querySelector("#content");
 
 startbtn.addEventListener("click", () => {
   startbtn.classList.add("gameon");
-  rockbtn.classList.remove("gameon");
-  paperbtn.classList.remove("gameon");
-  scissorbtn.classList.remove("gameon");
+  allRps.forEach((btn) => {
+    btn.classList.remove("gameon");
+  });
+  // rockbtn.classList.remove("gameon");
+  // paperbtn.classList.remove("gameon");
+  // scissorbtn.classList.remove("gameon");
+  block.children[2].textContent = "Choose:";
 });
+
+rockbtn.addEventListener("mouseover", () => {});
