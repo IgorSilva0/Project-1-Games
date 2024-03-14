470855ce835ad8fd3dd2d5d038afbb95259f61c7
Igor Silva <igorsilvauk@gmail.com>
2024-03-14 09:00:27 +0000

    code improved

--- public/games/rps.js
+++ public/games/rps.js
@@ -1,116 +1,125 @@
-const startbtn = document.querySelector("#startbtn");
+const startBtn = document.querySelector("#startbtn");
 const allRps = document.querySelectorAll(".rps");
 const block = document.querySelector("#content");
-let player = "";
+const linebreak = document.createElement("br");
+const playAgain = document.createElement("button");
+
+const btnAudio = new Audio("./sound/button.mp3");
+btnAudio.volume = 0.5;
+
+const roundInfo = block.children[1];
+const instruction = block.children[3];
+
+let playerChoice = "";
 let round = 1;
 
-// When press PLAY button
-startbtn.addEventListener("click", () => {
-  btnaudio.volume = 0.5;
-  btnaudio.play();
+startBtn.addEventListener("click", () => {
+  btnAudio.play();
   setTimeout(() => {
-    allRps.forEach((btn) => {
-      btn.classList.remove("gameon");
-    });
-    startbtn.classList.add("gameon");
-    block.children[1].textContent = `Round ${round}`; // h3
-    block.children[2].classList.add("gameon"); // img
-    block.children[3].textContent = "Choose:"; // h4
-  }, "200");
+    allRps.forEach((btn) => btn.classList.remove("gameon"));
+    startBtn.classList.add("gameon");
+    roundInfo.textContent = `Round ${round}`;
+    block.children[2].classList.add("gameon");
+    instruction.textContent = "Choose:";
+  }, 200);
 });
 
-// Listen to player Choice
-allRps.forEach((x) => {
-  x.addEventListener("mouseover", (z) => {
-    z.target.src =
-      z.target.src.slice(0, z.target.src.length - 4) + "2" + ".png";
+playAgain.addEventListener("click", () => {
+  allRps.forEach((btn) => btn.classList.remove("gameon"));
+  round++;
+  playAgain.remove();
+});
+
+allRps.forEach((btn) => {
+  btn.addEventListener("mouseover", (event) => {
+    event.target.src = event.target.src.replace(".png", "2.png");
   });
-  x.addEventListener("mouseout", (z) => {
-    z.target.src = z.target.src.slice(0, z.target.src.length - 5) + ".png";
+  btn.addEventListener("mouseout", (event) => {
+    event.target.src = event.target.src.replace("2.png", ".png");
   });
-  x.addEventListener("click", (z) => {
-    player = z.target.id;
+  btn.addEventListener("click", (event) => {
+    playerChoice = event.target.id;
     showChoice();
   });
 });
 
-// 3 2 1 GO
 async function showChoice() {
-  allRps.forEach((x) => x.classList.add("gameon"));
-  // create a first new img and style it
-  const handOne = document.createElement("img");
-  handOne.src = "../imgs/123.png";
-  handOne.classList.add("handOne");
-  handOne.id = "handOne";
-  // create a second new img and style it
-  const handTwo = document.createElement("img");
-  handTwo.src = "../imgs/321.png";
-  handTwo.classList.add("handTwo");
-  handTwo.id = "handTwo";
-  // append both imgs to content
-  block.appendChild(handOne);
-  block.appendChild(handTwo);
+  allRps.forEach((btn) => btn.classList.add("gameon"));
+  const handOne = createImage("../imgs/123.png", "handOne");
+  const handTwo = createImage("../imgs/321.png", "handTwo");
+
+  appendElements([handOne, handTwo]);
+  instruction.textContent = "3";
+  await delay(1000);
+  instruction.textContent = "2";
+  await delay(1000);
+  instruction.textContent = "1";
+  await delay(700);
+  instruction.textContent = "GOOOO!!!";
+
+  await delay(500);
+  removeElements([handOne, handTwo]);
   gameStart();
+  allRps.forEach((btn) => btn.remove());
+  choices(1, 2);
 }
 
-// Who wins?
 async function gameStart() {
   const rps = ["rock", "paper", "scissors"];
-  let random = Math.floor(Math.random() * 3);
-  let pcChoice = rps[random];
+  const random = Math.floor(Math.random() * 3);
+  const pcChoice = rps[random];
+  const winMessage = {
+    rock: "paper wins over rock",
+    paper: "scissors wins over paper",
+    scissors: "rock wins over scissors",
+  };
 
-  switch (pcChoice) {
-    // Computer chose ROCK
-    case "rock":
-      if (player === "paper") {
-        await updateScore();
-        await rankData();
-        console.log("You win! paper wins over rock");
-        break;
-      } else if (player === "rock") {
-        console.log(`It's a Draw (rock and rock)`);
-        break;
-      } else {
-        console.log(`Computer wins with rock`);
-        break;
-      }
-    // Computer chose PAPER
-    case "paper":
-      if (player === "scissors") {
-        await updateScore();
-        await rankData();
-        console.log("You win! scissors wins over paper");
-        break;
-      } else if (player === "paper") {
-        console.log(`It's a Draw (paper and paper)`);
-        break;
-      } else {
-        console.log(`Computer wins with paper`);
-        break;
-      }
-    // Computer chose SCISSORS
-    case "scissors":
-      if (player === "rock") {
-        await updateScore();
-        await rankData();
-        console.log("You win! rock wins over scissors");
-        break;
-      } else if (player === "scissors") {
-        console.log(`It's a Draw (scissors and scissors)`);
-        break;
-      } else {
-        console.log(`Computer wins with scissors`);
-        break;
-      }
-    // Default
-    default:
-      console.log("ERROR");
-      break;
+  if (playerChoice === pcChoice) {
+    console.log(`It's a Draw (${playerChoice} and ${pcChoice})`);
+  } else if (
+    (playerChoice === "rock" && pcChoice === "scissors") ||
+    (playerChoice === "paper" && pcChoice === "rock") ||
+    (playerChoice === "scissors" && pcChoice === "paper")
+  ) {
+    console.log(`You win! ${playerChoice} wins over ${pcChoice}`);
+    await updateScore();
+    await rankData();
+  } else {
+    console.log(`Computer wins! ${winMessage[pcChoice]}`);
   }
 }
 
 async function updateScore() {
-  await fetch("http://localhost:5432/scorerps/1?k=8*ej1^3d9K:J4zn136", {
+  await fetch("/scorerps/1?k=8*ej1^3d9K:J4zn136", {
     method: "PUT",
   });
+}
+
+async function choices(p, c) {
+  const choices = ["rockHand", "paperHand", "scissorsHand"];
+  const pc = createImage(`../imgs/${choices[p]}.png`, "choices");
+  const py = createImage(`../imgs/${choices[c]}.png`, "choices");
+
+  appendElements([pc, py, linebreak, playAgain]);
+  playAgain.id = "playAgain";
+  playAgain.textContent = "Again";
+}
+
+function createImage(src, id) {
+  const img = document.createElement("img");
+  img.src = src;
+  img.classList.add(id);
+  return img;
+}
+
+function appendElements(elements) {
+  elements.forEach((element) => block.appendChild(element));
+}
+
+function removeElements(elements) {
+  elements.forEach((element) => element.remove());
+}
+
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
--- public/main.js
+++ public/main.js
@@ -1,41 +1,50 @@
-const mainbox = document.querySelector("#main");
+const mainBox = document.querySelector("#main");
 const sideBar = document.querySelector("#ulSideBar");
-const nigth = document.querySelector("#nightMode");
-const day = document.querySelector("#lightMode");
-const topBar = document.querySelector("#topNavBar");
+const nightMode = document.querySelector("#nightMode");
+const lightMode = document.querySelector("#lightMode");
+const topNavBar = document.querySelector("#topNavBar");
 const contentOne = document.querySelector("#content");
 const contentTwo = document.querySelector("#content2");
 const rank = document.querySelector("#rank");
 const top1 = document.querySelector("#top1");
 const top2 = document.querySelector("#top2");
 const top3 = document.querySelector("#top3");
-const btnaudio = new Audio("./sound/button.mp3");
-const rankData = async () => {
-  await displayScore();
-};
 
-async function displayScore() {
-  const response = await fetch("http://localhost:5432/top3");
-  const data = await response
-    .json()
-    .then((data) => {
-      // TOP 1
-      top1.textContent = ` ${data.payload[0].score}`;
-      top1.previousElementSibling.textContent = `${data.payload[0].name}`;
-      top1.previousElementSibling.previousElementSibling.src = `${data.payload[0].img}`;
-      // TOP 2
-      top2.textContent = ` ${data.payload[1].score}`;
-      top2.previousElementSibling.textContent = `${data.payload[1].name}`;
-      top2.previousElementSibling.previousElementSibling.src = `${data.payload[1].img}`;
-
-      // TOP 3
-      top3.textContent = ` ${data.payload[2].score}`;
-      top3.previousElementSibling.textContent = `${data.payload[2].name}`;
-      top3.previousElementSibling.previousElementSibling.src = `${data.payload[2].img}`;
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return null;
+  }
 }
 
-rankData();
+async function displayRankData() {
+  const data = await fetchData("/top3");
+  if (!data) return;
+
+  try {
+    // TOP 1
+    top1.textContent = ` ${data.payload[0].score}`;
+    top1.previousElementSibling.textContent = `${data.payload[0].name}`;
+    top1.previousElementSibling.previousElementSibling.src = `${data.payload[0].img}`;
+
+    // TOP 2
+    top2.textContent = ` ${data.payload[1].score}`;
+    top2.previousElementSibling.textContent = `${data.payload[1].name}`;
+    top2.previousElementSibling.previousElementSibling.src = `${data.payload[1].img}`;
+
+    // TOP 3
+    top3.textContent = ` ${data.payload[2].score}`;
+    top3.previousElementSibling.textContent = `${data.payload[2].name}`;
+    top3.previousElementSibling.previousElementSibling.src = `${data.payload[2].img}`;
+  } catch (error) {
+    console.error("Error displaying rank data:", error);
+  }
+}
+
+displayRankData();
